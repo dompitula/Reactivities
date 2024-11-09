31b4b69a68fc33633f3b0072ae64e1a0b92ce98e
dompitula <pituladom@gmail.com>
2024-11-09 23:38:33 +0100

    End of Section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import { store, StoreContext } from './app/stores/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes.tsx'
@@ -9,7 +10,7 @@ import { router } from './app/router/Routes.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StoreContext.Provider value={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </StoreContext.Provider>
   </StrictMode>,
 )
