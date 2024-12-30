8240ad225f47fb5c951d246781fce574af294001
dompitula <pituladom@gmail.com>
2024-12-30 05:00:17 +0100

    Added Paging, Sorting, Filtering and Profile Activities showcase

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,30 +1,54 @@
 import { observer } from "mobx-react-lite"
-import { Grid } from "semantic-ui-react"
+import { Grid, GridColumn, Loader } from "semantic-ui-react"
 import { useStore } from "../../../app/stores/store"
 import ActivityList from './ActivityList'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import LoadingComponent from "../../../app/layout/LoadingComponent"
 import ActivityFilters from "./ActivityFilters"
+import { PagingParams } from "../../../app/models/pagination"
+import InfiniteScroll from "react-infinite-scroller"
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder"
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore()
-    const { loadActivities, activityRegistry } = activityStore
+    const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore
+    const [loadingNext, setLoadingNext] = useState(false)
+
+    function handleGetNext() {
+        setLoadingNext(true)
+        setPagingParams(new PagingParams(pagination!.currentPage + 1))
+        loadActivities().then(() => setLoadingNext(false))
+    }
 
     useEffect(() => {
         if (activityRegistry.size === 0) loadActivities()
     }, [loadActivities, activityRegistry.size])
 
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading Activities...' />
-
     return (
         <Grid>
-            <Grid.Column width='10'>
-                <ActivityList />
-            </Grid.Column>
-            <Grid.Column width='6'>
+            <GridColumn width='10'>
+                {activityStore.loadingInitial && activityRegistry.size === 0 && !loadingNext ? (
+                    <>
+                        <ActivityListItemPlaceholder />
+                        <ActivityListItemPlaceholder />
+                    </>
+                ) : (
+                    <InfiniteScroll
+                        pageStart={0}
+                        loadMore={handleGetNext}
+                        hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                        initialLoad={false}
+                    >
+                        <ActivityList />
+                    </InfiniteScroll>
+                )}
+            </GridColumn>
+            <GridColumn width='6'>
                 <ActivityFilters />
-            </Grid.Column>
-
+            </GridColumn>
+            <GridColumn width={10}>
+                <Loader active={loadingNext} />
+            </GridColumn>
         </Grid>
     )
 })
--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,22 +1,23 @@
 import { observer } from 'mobx-react-lite'
-import { Tab, TabPane } from 'semantic-ui-react'
+import { Tab } from 'semantic-ui-react'
 import { Profile } from '../../app/models/profile'
 import ProfilePhotos from './ProfilePhotos'
 import ProfileAbout from './ProfileAbout'
 import ProfileFollowings from './ProfileFollowings'
 import { useStore } from '../../app/stores/store'
+import ProfileActivities from './ProfileActivities'
 
 interface Props {
     profile: Profile
 }
 
 export default observer(function ProfileContent({ profile }: Props) {
-    const {profileStore} = useStore()
+    const { profileStore } = useStore()
 
     const panes = [
         { menuItem: 'About', render: () => <ProfileAbout /> },
         { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
-        { menuItem: 'Events', render: () => <TabPane>Events Content</TabPane> },
+        { menuItem: 'Events', render: () => <ProfileActivities /> },
         { menuItem: 'Followers', render: () => <ProfileFollowings /> },
         { menuItem: 'Following', render: () => <ProfileFollowings /> }
     ]
